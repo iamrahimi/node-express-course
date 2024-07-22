72849208ef4b0be40ad7b8d83c44fe362d3941e1
iamrahimi <r6himi@gmail.com>
2024-07-22 09:48:00 -0700

    Week 4 Assignment

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,43 +1,22 @@
 const express = require('express');
-const {products} = require("./data");
+const {products, people} = require("./data");
+const peopleRouter = require('./routes/peoples');
 
 const app = express();
+app.use(express.static('./methods-public'));
 
 
-app.get('/', (req, res) => {
-    res.status(200).json({message:"it works well"});
-})
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
-app.get('/api/v1/products/:productID', (req, res) => {
-    const idToFind = parseInt(req.params.productID); 
-    let product = products.find((p) => p.id === idToFind);
-    if(product === undefined || product.length == 0)
-    {
-        product = {
-            message: "That product was not found"
-        }
-    }
 
-    res.status(200).json(product);
-})
+app.use('/api/v1/peoples', peopleRouter);
 
-app.get('/api/v1/query', (req, res, next) => {
-    
-    const search = req.query.search;
-    /**
-     * Filter array items based on search criteria (query)
-     */
-    function SearchItem(arr, query) {
-        return arr.filter((el) => el.desc.toLowerCase().includes(query.toLowerCase()));
-    }    
-    res.status(200).json(SearchItem(products,search));
-})
 
 
 app.get('*', (req,res) => {
     res.status(404).json('page not found');
 })
-
 
 app.listen(3000, () => {
     console.log('The server stated at port 3000');
